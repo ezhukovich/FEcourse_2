d09c77820bba1ae57272602c7234395e132a1e4c
Пользователь <kb191_nvv@student.ztu.edu.ua>
2020-10-17 01:02:57 +0300

    Nekoz: Homework_6 done

--- Nekoz/XO/scripts/XO.js
+++ Nekoz/XO/scripts/XO.js
@@ -9,17 +9,21 @@ let step = 0;
 
 document.getElementById("block").onclick = function(event) {
     console.log(event);
+    if(event.target.textContent.length === 0){
+        if(event.target.className === 'block' ){
+            console.log();
+            console.log(event.target);
+            if(step %2 === 0){
+                event.target.innerHTML = 'O';
 
-    if(event.target.className === 'block'){
-        if(step %2 === 0){
-            event.target.innerHTML = 'O';
-
+            }
+            else{
+                event.target.innerHTML = 'X';
+            }
+            step++;
         }
-        else{
-            event.target.innerHTML = 'X';
-        }
-        step++;
     }
+
 
     if(document.getElementById(1).innerHTML === 'O' && document.getElementById(2).innerHTML === 'O' && document.getElementById(3).innerHTML === 'O' || document.getElementById(4).innerHTML === 'O' && document.getElementById(5).innerHTML === 'O' && document.getElementById(6).innerHTML === 'O' || document.getElementById(7).innerHTML === 'O' && document.getElementById(8).innerHTML === 'O' && document.getElementById(9).innerHTML === 'O' || document.getElementById(1).innerHTML === 'O' && document.getElementById(5).innerHTML === 'O' && document.getElementById(9).innerHTML === 'O' || document.getElementById(3).innerHTML === 'O' && document.getElementById(5).innerHTML === 'O' && document.getElementById(7).innerHTML === 'O')
     {
