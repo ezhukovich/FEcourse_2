1ca37d18207c95828705c974dd397eedda621509
Pushkin <respect4me@ukr.net>
2020-12-01 16:07:37 +0200

    Pushkin:Homework19 cross

--- Pushkin/L22-Homework/script10.js
+++ Pushkin/L22-Homework/script10.js
@@ -1,5 +1,5 @@
 let date1 = prompt("YYYY-MM-DD");
 today = Date.parse(date1);
-tomorrowParse = today + 86400000; //проанализировал парс соседних дней 
+tomorrowParse = today + 86400000; //проанализировал парс соседних дней и добавил
 let tomorrowDate = new Date(tomorrowParse);
 alert(tomorrowDate);
